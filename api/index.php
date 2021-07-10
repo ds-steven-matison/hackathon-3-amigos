c5b24dbb1c202a431b00ef41767c2e368017a370
Steven <steven.matison@datastax.com>
2021-07-09 22:35:45 -0400

    Cleanup API

--- api/index.php
+++ api/index.php
@@ -1,6 +1,5 @@
 <?php header("Access-Control-Allow-Origin: *");
 
-
 include dirname(realpath('.'))."/env.php";
 
 if($NIFI_URL == '' && $ASTRA_TOKEN == '') {
@@ -99,9 +98,6 @@ if($_SERVER['REQUEST_METHOD']=="POST") {
 		$request = curl_init();
 
 			$url = $STARGATE_URL.'keyspaces/' . $KEYSPACE . '/' . $table_name;
-			// this should now be the NIFI URL at DC1
-			// need api path for post new row into local database biometrics_by_voter
-			//$url = 'http://143.198.122.36:8081/pulsar-producer';
 			$data_array = array(
 				'voter_uuid' => uuid(),
 				'face_photo_1' => 'data:image/jpg;base64,'.tintTheImage($image1,'rgb('.$APP_R.', '.$APP_B.', '.$APP_G.')'),
@@ -139,28 +135,6 @@ if($_SERVER['REQUEST_METHOD']=="POST") {
 		else {
 			echo json_encode(['code'=>404, 'msg'=>$errorMSG]);
 		}
-	} else { // this was the original test for pulsar-producer
-
-		$source_images = array('assinatura_machado_de_assis-dominio-publico-14kb.jpg','machado_de_assis_dominio-publico-84kb.jpg','machado_de_assis_dominio-publico-9kb.jpg','machado_de_assis_dominio-publico-12kb.jpg','fingerprint-22kb.png');
-
-		foreach($source_images as $image) { 
-			$image_data = 'data:image/jpg;base64,'.base64_encode(file_get_contents('/usr/share/nginx/html/images/'.$image));
-			$request = curl_init();
-			$url = 'http://localhost:8081/pulsar-producer';
-			$data_array = array();
-			$data_array['image_name'] = $image;
-			$data_array['image_data'] = $image_data;
-			$data_json = json_encode($data_array);
-			curl_setopt($request, CURLOPT_URL, $url);
-			curl_setopt($request, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
-			curl_setopt($request, CURLOPT_CUSTOMREQUEST, 'POST');
-			curl_setopt($request, CURLOPT_POSTFIELDS,$data_json);
-			curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-			$response  = curl_exec($request);
-			curl_close($request);
-			echo $response;
-		}
-
 	}
 
 } else {
